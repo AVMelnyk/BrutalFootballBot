679ba39098ba0c82ff0ff03ad643239f595b204c
AVMelnyk <melnyk.andreew@gmail.com>
2017-02-27 21:57:18 +0200

    new conf

--- src/main/java/MemeParser.java
+++ src/main/java/MemeParser.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class MemeParser {
     private static ArrayList<String> picSize = new ArrayList<String>();
     public static void main(String[] args) throws IOException{
-		putMemeInDB;
+		putMemeInDB();
     }
     public  static void putMemeInDB()throws IOException{
 
